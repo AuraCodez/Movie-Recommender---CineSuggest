183ffae26d36eec60c43712ad5884c39dfcd75b9
Ryan <86922669+AuraCodez@users.noreply.github.com>
2023-09-04 20:07:26 -0400

    Added Movie Object for future mapping of JSON Response

--- src/main/java/com/example/movieservicing/movierecs/Controller/MovieRecController.java
+++ src/main/java/com/example/movieservicing/movierecs/Controller/MovieRecController.java
@@ -1,17 +1,31 @@
 package com.example.movieservicing.movierecs.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.movieservicing.movierecs.Model.Movie;
 import com.example.movieservicing.movierecs.Service.RecService;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieRecController {
     
     @Autowired
     public RecService movieService;
 
-    
 
+    @GetMapping("/movies/recommendations")
+    public List<Movie> getRecommendations() {
+        return null; 
+    }
+
+    @GetMapping("/movies/ratings")
+    public int getRatings() {
+        return 0;
+    }
     
 }
--- src/main/java/com/example/movieservicing/movierecs/Model/Movie.java
+++ src/main/java/com/example/movieservicing/movierecs/Model/Movie.java
@@ -1,5 +1,21 @@
 package com.example.movieservicing.movierecs.Model;
 
 public class Movie {
-    
+    private double rating;
+    private String movieTitle;
+
+    public Movie(double rating, String movieTitle) {
+        this.rating = rating;
+        this.movieTitle = movieTitle;
+    }
+
+    public double getMovieRating() {
+        return this.rating;
+    }
+
+    public String getMovieTitle() {
+        return this.movieTitle;
+    }
+
+
 }
