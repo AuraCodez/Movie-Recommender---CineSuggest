d91599c2a19e59ba5d650b78293ddbaf89142f50
Ryan <86922669+AuraCodez@users.noreply.github.com>
2023-09-17 21:27:37 -0400

    update .gitignore file and added API key

--- src/main/java/com/example/movieservicing/movierecs/Controller/MovieRecController.java
+++ src/main/java/com/example/movieservicing/movierecs/Controller/MovieRecController.java
@@ -17,7 +17,6 @@ public class MovieRecController {
     @Autowired
     public RecService movieService;
 
-
     @GetMapping("/movies/recommendations")
     public List<Movie> getRecommendations() {
         return null; 
