a8a4d2ffd85756cb15316070c108a72a6657593b
Ryan <86922669+AuraCodez@users.noreply.github.com>
2023-10-21 19:44:25 -0400

    added movie model
    this class is a DTO for deserializing the api response and also acts as a document in the mongodb database.

--- src/main/java/com/example/movieservicing/movierecs/Model/Movie.java
+++ src/main/java/com/example/movieservicing/movierecs/Model/Movie.java
@@ -1,18 +1,86 @@
 package com.example.movieservicing.movierecs.Model;
 
 import java.util.List;
+import org.springframework.data.annotation.Id;
 
 public class Movie {
+    @Id
+    private String mongoDbID;
     private boolean adult;
-    private String backdropPath;
     private List<Integer> genreIds;
-    private int id;
-    private String originalLanguage;
+    private Integer id;
     private String originalTitle;
     private String overview;
     private double popularity;
     private String releaseDate;
     private int voteAverage;
     private int voteCount;
+
+    public Movie() {
+    }
+
+    public Movie(Integer id, String originalTitle, String overview, List<Integer> genreIds, double popularity,
+            String releaseDate, int voteCount, int voteAverage) {
+        this.id = id;
+        this.originalTitle = originalTitle;
+        this.overview = overview;
+        this.genreIds = genreIds;
+        this.popularity = popularity;
+        this.releaseDate = releaseDate;
+        this.voteCount = voteCount;
+        this.voteAverage = voteAverage;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getTitle() {
+        return originalTitle;
+    }
+
+    public void setTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public int getVoteAverage() {
+        return voteAverage;
+    }
+
+    public List<Integer> getGenreIDS() {
+        return genreIds;
+    }
 
 }
