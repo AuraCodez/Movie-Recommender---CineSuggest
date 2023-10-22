8f23ea771c53097fb0200451f424408bd31c6663
Ryan <86922669+AuraCodez@users.noreply.github.com>
2023-10-21 23:09:12 -0400

    implemented repository class
    Added in repository class for User and Movie, will add in TV model and TV repository later.

--- src/main/java/com/example/movieservicing/movierecs/Repository/MovieRepository.java
+++ src/main/java/com/example/movieservicing/movierecs/Repository/MovieRepository.java
@@ -2,7 +2,13 @@ package com.example.movieservicing.movierecs.Repository;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class MovieRepository {
+import com.example.movieservicing.movierecs.Model.Movie;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, String> {
+    Movie findByOriginalTitle(String originalTitle);
+    Movie findByPopularity(double popularity);
+    
 }
--- src/main/java/com/example/movieservicing/movierecs/Repository/UserRepository.java
+++ src/main/java/com/example/movieservicing/movierecs/Repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.example.movieservicing.movierecs.Repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.movieservicing.movierecs.Model.User;
+
 @Repository
-public class UserRepository {
-    
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUserName(String userName);
+
+    User findByEmail(String email);
 }
