503a868d7d3bcf8d93b84cc9a77f88d9aa7730f9
Ryan <86922669+AuraCodez@users.noreply.github.com>
2023-12-23 22:57:32 -0500

    Remove duplicate Service directory

--- src/main/java/com/example/movieservicing/movierecs/Service/TmdbEntityService.java
+++
@@ -1,9 +0,0 @@
-package com.example.movieservicing.movierecs.service;
-
-import java.util.Map;
-
-public interface TmdbEntityService<T> {
-    Map<String, Object>  fetchPopular();
-    Map<String, Object>  fetchTrending();
-
-}
--- src/main/java/com/example/movieservicing/movierecs/Service/UserDetailsService.java
+++
@@ -1,5 +0,0 @@
-package com.example.movieservicing.movierecs.service;
-
-public class UserDetailsService {
-    
-}
