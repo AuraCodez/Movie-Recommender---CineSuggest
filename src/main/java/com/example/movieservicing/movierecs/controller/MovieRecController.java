3243a23859378b926518b99a59e76139ab20a4f2
Ryan <86922669+AuraCodez@users.noreply.github.com>
2023-12-23 22:59:41 -0500

    Track lowercase controller directory

---
+++ src/main/java/com/example/movieservicing/movierecs/controller/MovieRecController.java
@@ -0,0 +1,37 @@
+package com.example.movieservicing.movierecs.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.movieservicing.movierecs.service.TmdbMovieService;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieRecController {
+
+    private final TmdbMovieService movieService;
+
+    @Autowired
+    public MovieRecController(TmdbMovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/recommendations")
+    public ResponseEntity<Map<String, Object>> getPopularMovies() {
+        Map<String, Object> popularMovies = movieService.fetchPopular();
+        return ResponseEntity.ok(popularMovies);
+
+    }
+
+    @GetMapping("/trending")
+    public ResponseEntity<Map<String, Object>> getTrendingForMovies() {
+        Map<String, Object> trendingMovies = movieService.fetchTrending();
+        return ResponseEntity.ok(trendingMovies);
+    }
+
+}
---
+++ src/main/java/com/example/movieservicing/movierecs/controller/TvRecController.java
@@ -0,0 +1,36 @@
+package com.example.movieservicing.movierecs.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.movieservicing.movierecs.service.TmdbTvService;
+
+@RestController
+@RequestMapping("/tv")
+public class TvRecController {
+
+    private final TmdbTvService tvService;
+
+    @Autowired
+    public TvRecController(TmdbTvService tvService) {
+        this.tvService = tvService;
+    }
+
+    @GetMapping("/recommendations")
+    public ResponseEntity<Map<String, Object>> getPopularTvShows() {
+        Map<String, Object> popularTvShows = tvService.fetchPopular();
+        return ResponseEntity.ok(popularTvShows);
+    }
+
+    @GetMapping("/trending")
+    public ResponseEntity<Map<String, Object>> getTrendingTvForUsers() {
+        Map<String, Object> trendingTvShows = tvService.fetchTrending();
+        return ResponseEntity.ok(trendingTvShows);
+    }
+
+}
