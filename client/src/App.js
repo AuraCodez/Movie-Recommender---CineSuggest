5a11cdb70deddfb93a04175ed9a525d4f585f321
Ryan <86922669+AuraCodez@users.noreply.github.com>
2024-02-27 21:28:38 -0500

    Updated app page
    - Added restart functionality
    - Added some CSS to make it more appealing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,20 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
+  const scoringSystem = {
+    "Happy": 10,
+    "Neutral": 5,
+    "Sad": 2,
+    "Just myself": 5,
+    "With a group of friends and family": 7,
+    "Action": 8,
+    "Adventure": 8,
+    "Comedy": 10,
+    "Romance": 4,
+    "Science Fiction": 2
+  }
+
+
   const questions = [
     {
       questionText: 'How are you feeling today?',
@@ -32,23 +46,39 @@ function App() {
   const questionsSize = questions.length;
   const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  const [suggestions, setSuggestions] = useState([]);
 
   const handleAnswerButtonClick = (answerText) => {
     setUserAnswers(prevAnswers => [...prevAnswers, answerText]);
-    const nextQuestion = currentQuestionIdx + 1;
-    if (nextQuestion < questionsSize) {
-      setCurrentQuestionIdx(nextQuestion);
+    const nextQuestionIdx = currentQuestionIdx + 1;
+    if (nextQuestionIdx <= questionsSize) {
+      setCurrentQuestionIdx(nextQuestionIdx);
     } else {
-      // Generate the suggestions here, and display the suggestions
+      // Nothing yet
+
     }
+  }
+
+  const restartQuiz = () => {
+    setCurrentQuestionIdx(0)
+    setUserAnswers([]);
   }
 
 
   return (
     <div className='app'>
       {currentQuestionIdx >= questionsSize ? (
-        <div className='score-section'>End of the Questionaire</div>
+        <div className='score-section'>
+          <div>End of the Questionaire</div>
+          <div className='user-answers'>
+            <h3>Your Answers:</h3>
+            <ul>
+              {userAnswers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+          </div>
+          <button onClick={restartQuiz}>Restart</button>
+        </div>
       ) : (
         <>
           <div className='question-section'>
@@ -59,7 +89,9 @@ function App() {
           </div>
           <div className='answer-section'>
             {questions[currentQuestionIdx].answerOptions.map((answerOption, index) => (
-              <button onClick={() => handleAnswerButtonClick(answerOption.answerText)} key={index}>
+              <button
+                className="button"
+                onClick={() => handleAnswerButtonClick(answerOption.answerText)} key={index}>
                 {answerOption.answerText}
               </button>
             ))}
