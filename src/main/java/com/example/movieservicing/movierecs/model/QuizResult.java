d1127179ae3d481bec2d693875c7681972e698e6
Ryan <86922669+AuraCodez@users.noreply.github.com>
2024-03-01 23:03:54 -0500

    CORS configuration
    - Added CORS configuration for Spring Boot
    - Added quiz result DTO model

---
+++ src/main/java/com/example/movieservicing/movierecs/model/QuizResult.java
@@ -0,0 +1,49 @@
+package com.example.movieservicing.movierecs.model;
+
+public class QuizResult {
+    private int quizScore;
+    private String mood;
+    private String occasion;
+    private String preferredGenre;
+
+    public QuizResult(int quizScore, String mood, String occasion, String preferredGenre) {
+        this.quizScore = quizScore;
+        this.mood = mood;
+        this.occasion = occasion;
+        this.preferredGenre = preferredGenre;
+    }
+
+    public int getQuizScore() {
+        return this.quizScore;
+    }
+
+    public void setQuizcore(int quizScore) {
+        this.quizScore = quizScore;
+
+    }
+
+    public String getMood() {
+        return this.mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getOccasion() {
+        return this.occasion;
+    }
+
+    public void setOccasion(String occasion) {
+        this.occasion = occasion;
+    }
+
+    public String getPreferredGenre() {
+        return this.preferredGenre;
+    }
+
+    public void setPreferredGenre(String genre) {
+        this.preferredGenre = genre;
+    }
+
+}
